0a99e0bb0dd13f77ebf898f1c678616b0084cb84
Jin-K-Yang <ken8989580@gmail.com>
2023-08-28 17:18:49 +0000

    complete modification

--- src/FlashLoanReceiverBase.sol
+++ src/FlashLoanReceiverBase.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20  } from "Interfaces.sol";
-import { SafeERC20, SafeMath } from "Libraries.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import {SafeMath} from "openzeppelin/utils/math/SafeMath.sol";
+import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20} from "./Interfaces.sol";
 
 abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
-  using SafeERC20 for IERC20;
-  using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
-  ILendingPoolAddressesProvider internal _addressesProvider;
-  ILendingPool internal _lendingPool;
+    ILendingPoolAddressesProvider internal _addressesProvider;
+    ILendingPool internal _lendingPool;
 
-  constructor(ILendingPoolAddressesProvider provider) public {
-    _addressesProvider = provider;
-    _lendingPool = ILendingPool(ILendingPoolAddressesProvider(provider).getLendingPool());
-  }
+    constructor(ILendingPoolAddressesProvider provider) {
+        _addressesProvider = provider;
+        _lendingPool = ILendingPool(ILendingPoolAddressesProvider(provider).getLendingPool());
+    }
 
-  receive() external payable {}
+    receive() external payable {}
 }
